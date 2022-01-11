ecebc93cf7b2d2bd02ef0fd6b940bd3e114ef505
Jon Jackson <jon.jackson@perficient.com>
2022-01-10 21:30:07 -0500

    adding sidebar basic layout

--- notetab/src/components/Homepage.js
+++ notetab/src/components/Homepage.js
@@ -4,13 +4,21 @@ import "../style.css";
 function Homepage() {
   return (
     <>
-      <nav id="tabs">
+      <main id="main">
         <h1 id="greeting">sup dude</h1>
         <div class="folder-tab selected">
           <span>Time entries</span>
         </div>
-      </nav>
-      <div id="page">Lorem Ipsum dolor sit amet ...asdfasdf</div>
+      </main>
+
+      <div id="page">Lorem Ipsum dolor sit amet ...asdfasdf </div>
+      <div class="sidenav">
+        <input class="search-input" type="text" placeholder="Search.." />
+        <a href="">Note 1</a>
+        <a href="">Note 2</a>
+        <a href="">Note 3</a>
+        <a href="">Note 4</a>
+      </div>
     </>
   );
 }

4860a4ef2543e3a86c7e22df0712967cdb560c4f
Jon Jackson <jon.jackson@perficient.com>
2022-01-10 22:04:27 -0500

    need to add footer. need to take up more space in bottom <div>

--- notetab/src/components/Homepage.js
+++ notetab/src/components/Homepage.js
@@ -4,14 +4,46 @@ import "../style.css";
 function Homepage() {
   return (
     <>
-      <main id="main">
-        <h1 id="greeting">sup dude</h1>
-        <div class="folder-tab selected">
-          <span>Time entries</span>
-        </div>
-      </main>
+      <h1 id="greeting">sup dude</h1>
+      <div class="folder-tab selected">
+        <span>Time entries</span>
+      </div>
 
-      <div id="page">Lorem Ipsum dolor sit amet ...asdfasdf </div>
+      <div id="page">
+        Lorem Ipsum dolor sit amet ...asdfasdf
+        {/* <footer id="footer">
+          <p>
+            Author: Hege Refsnes
+            <br />
+            <a href="mailto:hege@example.com">hege@example.com</a>
+          </p>
+        </footer> */}
+      </div>
+      <div id="footer">
+        <footer class="footer-class">
+          <p>
+            Author: Who Dis
+            <br />
+            <a href="mailto:hege@example.com">whod@example.com</a>
+          </p>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque corrupti quos
+            dolores et quas molestias excepturi sint occaecati cupiditate non
+            provident, similique sunt in culpa qui officia deserunt mollitia
+            animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est
+            eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+            facere possimus, omnis voluptas assumenda est, omnis dolor
+            repellendus. Temporibus autem quibusdam et aut officiis debitis aut
+            rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint
+            et molestiae non recusandae. Itaque earum rerum hic tenetur a
+            sapiente delectus, ut aut reiciendis voluptatibus maiores alias
+            consequatur aut perferendis doloribus asperiores repellat.
+          </p>
+        </footer>
+      </div>
+
       <div class="sidenav">
         <input class="search-input" type="text" placeholder="Search.." />
         <a href="">Note 1</a>
@@ -19,6 +51,9 @@ function Homepage() {
         <a href="">Note 3</a>
         <a href="">Note 4</a>
       </div>
+      {/* <div>
+        <p>Author: Hege Refsnes</p>
+      </div> */}
     </>
   );
 }
